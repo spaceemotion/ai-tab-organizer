06a6651f6c1aed9764bf0d5332ca90dc50fe952e
Leonie Gräßel <hello@spaceemotion.net>
2023-08-05 21:59:23 +0200

    Add tailwindcss support with watch commands
    - Add tailwindcss for styling all pages
    - Add new build and watch scripts to build new TS+CSS combo

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
--- tsup.config.ts
+++ tsup.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     'src/background.ts',
     'src/options.ts',
   ],
+
   publicDir: 'public',
 
   noExternal: [
