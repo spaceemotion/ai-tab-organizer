e1062f6de97698e2bb2d4d9e38d7adc366bec686
Leonie Gräßel <hello@spaceemotion.net>
2023-08-06 00:16:46 +0200

    Use async/await in all actions

--- src/actions/closeBlankPages.ts
+++ src/actions/closeBlankPages.ts
@@ -1,14 +1,13 @@
 import { mapTabIds } from "../utils/tabs";
 
 export default async function closeBlankPages() {
-  chrome.tabs.query({ currentWindow: false }, (tabs) => {
-    const blankTabs = tabs.filter((tab) => tab.url === 'chrome://newtab/');
+  const tabs = await chrome.tabs.query({});
+  const blankTabs = tabs.filter((tab) => tab.url === 'chrome://newtab/');
 
-    console.log(
-      `Found ${blankTabs.length} blank tabs, closing`,
-      blankTabs.map((tab) => tab.url),
-    );
+  console.log(
+    `Found ${blankTabs.length} blank tabs, closing`,
+    blankTabs.map((tab) => tab.url),
+  );
 
-    chrome.tabs.remove(mapTabIds(blankTabs));
-  });
+  chrome.tabs.remove(mapTabIds(blankTabs));
 }
--- src/actions/deduplicateTabs.ts
+++ src/actions/deduplicateTabs.ts
@@ -1,16 +1,16 @@
 import { mapTabIds } from "../utils/tabs";
 
 export default async function deduplicateTabs() {
-  chrome.tabs.query({ currentWindow: false }, (tabs) => {
-    const urls = tabs.map((tab) => tab.url);
-    const duplicates = urls.filter((url, index) => urls.indexOf(url) !== index);
-    const duplicateTabs = tabs.filter((tab) => duplicates.includes(tab.url));
+  const tabs = await chrome.tabs.query({});
+  const urls = tabs.map((tab) => tab.url);
 
-    console.log(
-      `Found ${duplicateTabs.length} duplicate tabs, closing`,
-      duplicateTabs.map((tab) => tab.url),
-    );
+  const duplicates = urls.filter((url, index) => urls.indexOf(url) !== index);
+  const duplicateTabs = tabs.filter((tab) => duplicates.includes(tab.url));
 
-    chrome.tabs.remove(mapTabIds(duplicateTabs));
-  });
+  console.log(
+    `Found ${duplicateTabs.length} duplicate tabs, closing`,
+    duplicateTabs.map((tab) => tab.url),
+  );
+
+  chrome.tabs.remove(mapTabIds(duplicateTabs));
 }
