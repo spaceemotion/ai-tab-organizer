4e9e33eed7d1f3c93ac16c37240cd2b5d4ce57ca
Leonie Gräßel <hello@spaceemotion.net>
2023-08-06 00:21:43 +0200

    Add internal "keeplist" for duplicate tabs
    Services like ChatGPT don't update their URL when you start chatting.
    This will create many "duplicate" tabs, even though they aren't.

--- src/actions/deduplicateTabs.ts
+++ src/actions/deduplicateTabs.ts
@@ -1,11 +1,16 @@
 import { mapTabIds } from "../utils/tabs";
 
+// Keep these URLs open, as they don't keep their state in the URl
+const keepUrlsRegex = /(chat\.openai\.com)/i;
+
 export default async function deduplicateTabs() {
   const tabs = await chrome.tabs.query({});
   const urls = tabs.map((tab) => tab.url);
 
   const duplicates = urls.filter((url, index) => urls.indexOf(url) !== index);
-  const duplicateTabs = tabs.filter((tab) => duplicates.includes(tab.url));
+  const duplicateTabs = tabs.filter((tab) => (
+    duplicates.includes(tab.url) && !keepUrlsRegex.test(tab.url!)
+  ));
 
   console.log(
     `Found ${duplicateTabs.length} duplicate tabs, closing`,
