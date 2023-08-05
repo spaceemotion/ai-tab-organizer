552e0382d1949014a0c9391168c3aee99771667a
Leonie Gräßel <hello@spaceemotion.net>
2023-08-05 22:48:11 +0200

    Add deduplicate pages action
    Resolves #4

--- src/background.ts
+++ src/background.ts
@@ -20,6 +20,16 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
 
       chrome.tabs.remove(mapTabIds(blankTabs));
     });
+  } else if (message.action === 'action-deduplicate') {
+    chrome.tabs.query({ currentWindow: forCurrentWindow }, (tabs) => {
+      const urls = tabs.map((tab) => tab.url);
+      const duplicates = urls.filter((url, index) => urls.indexOf(url) !== index);
+      const duplicateTabs = tabs.filter((tab) => duplicates.includes(tab.url));
+
+      console.log(`Found ${duplicateTabs.length} duplicate tabs, closing`);
+
+      chrome.tabs.remove(mapTabIds(duplicateTabs));
+    });
   } else if (message.action === 'action-ai') {
     await organizeViaAi();
   }
